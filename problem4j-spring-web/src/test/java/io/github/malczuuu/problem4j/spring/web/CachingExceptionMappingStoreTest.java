2332c3e7f898c2ce634b2706c987bbfe936bf8b0
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-10-05 13:03:48 +0200

    Add test for caching

--- problem4j-spring-web/src/test/java/io/github/malczuuu/problem4j/spring/web/CachingExceptionMappingStoreTest.java
+++ problem4j-spring-web/src/test/java/io/github/malczuuu/problem4j/spring/web/CachingExceptionMappingStoreTest.java
@@ -1,13 +1,13 @@
-package io.github.malczuuu.problem4j.spring.web.mapping;
+package io.github.malczuuu.problem4j.spring.web;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.github.malczuuu.problem4j.core.Problem;
-import io.github.malczuuu.problem4j.spring.web.CachingExceptionMappingStore;
-import io.github.malczuuu.problem4j.spring.web.ExceptionMappingStore;
+import io.github.malczuuu.problem4j.spring.web.mapping.ExceptionMapping;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
@@ -43,19 +43,19 @@ class CachingExceptionMappingStoreTest {
     }
   }
 
-  private ExceptionMappingStore registry;
+  private ExceptionMappingStore store;
 
   @BeforeEach
   void setUp() {
     ExceptionMapping baseMapping = new TestMapping(MyBaseException.class);
     ExceptionMapping subMapping = new TestMapping(MySubException.class);
     ExceptionMapping otherMapping = new TestMapping(OtherException.class);
-    registry = new CachingExceptionMappingStore(List.of(baseMapping, subMapping, otherMapping));
+    store = new CachingExceptionMappingStore(List.of(baseMapping, subMapping, otherMapping));
   }
 
   @Test
   void testExactMatch() {
-    Optional<ExceptionMapping> result = registry.findMapping(MySubException.class);
+    Optional<ExceptionMapping> result = store.findMapping(MySubException.class);
 
     assertTrue(result.isPresent());
     assertEquals(MySubException.class, result.get().getExceptionClass());
@@ -63,7 +63,7 @@ class CachingExceptionMappingStoreTest {
 
   @Test
   void testClosestSuperclassMatch() {
-    Optional<ExceptionMapping> result = registry.findMapping(MySubSubException.class);
+    Optional<ExceptionMapping> result = store.findMapping(MySubSubException.class);
 
     assertTrue(result.isPresent());
     assertEquals(MySubException.class, result.get().getExceptionClass());
@@ -71,7 +71,7 @@ class CachingExceptionMappingStoreTest {
 
   @Test
   void testBaseClassMatch() {
-    Optional<ExceptionMapping> result = registry.findMapping(MyBaseException.class);
+    Optional<ExceptionMapping> result = store.findMapping(MyBaseException.class);
 
     assertTrue(result.isPresent());
     assertEquals(MyBaseException.class, result.get().getExceptionClass());
@@ -79,8 +79,33 @@ class CachingExceptionMappingStoreTest {
 
   @Test
   void testNoMatchForUnrelatedException() {
-    Optional<ExceptionMapping> result = registry.findMapping(Exception.class);
+    Optional<ExceptionMapping> result = store.findMapping(Exception.class);
 
     assertTrue(result.isEmpty());
+  }
+
+  @Test
+  void givenExceptionMappingStore_whenFindingMapping_thenCacheIsUsed() {
+    AtomicInteger counter = new AtomicInteger(0);
+    ExceptionMapping mapping =
+        new TestMapping(MyBaseException.class) {
+          @Override
+          public Class<? extends Exception> getExceptionClass() {
+            counter.incrementAndGet();
+            return super.getExceptionClass();
+          }
+        };
+
+    CachingExceptionMappingStore store = new CachingExceptionMappingStore(List.of(mapping));
+
+    Optional<ExceptionMapping> firstLookup = store.findMapping(MyBaseException.class);
+
+    assertTrue(firstLookup.isPresent(), "mapping should be present");
+    assertEquals(1, counter.get(), "counter should increment once");
+
+    Optional<ExceptionMapping> secondLookup = store.findMapping(MyBaseException.class);
+
+    assertTrue(secondLookup.isPresent(), "mapping should still be present");
+    assertEquals(1, counter.get(), "counter should not increment again");
   }
 }
