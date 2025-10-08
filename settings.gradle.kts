796b5dec5a53cbc0f204ac85c7049d940f4e7cc5
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-10-08 23:02:45 +0200

    Add org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+}
+
 rootProject.name = "problem4j-spring"
 
 include(":problem4j-spring-bom")
