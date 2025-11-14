6db52656ee69d5007f40bc11636d70eeb907d557
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-14 13:31:51 +0100

    Add tests for '*.enabled' properties

--- problem4j-spring-web/src/test/java/io/github/malczuuu/problem4j/spring/web/ProblemAutoConfigurationTest.java
+++ problem4j-spring-web/src/test/java/io/github/malczuuu/problem4j/spring/web/ProblemAutoConfigurationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.github.malczuuu.problem4j.spring.web.app.TestApp;
 import io.github.malczuuu.problem4j.spring.web.resolver.ProblemResolverConfiguration;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,15 +12,50 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(classes = {TestApp.class})
 class ProblemAutoConfigurationTest {
 
-  @Autowired(required = false)
-  private ProblemAutoConfiguration problemAutoConfiguration;
+  @SpringBootTest(classes = {TestApp.class})
+  @Nested
+  class WithEnabled {
 
-  @Autowired(required = false)
-  private ProblemResolverConfiguration problemResolverConfiguration;
+    @Autowired(required = false)
+    private ProblemAutoConfiguration problemAutoConfiguration;
 
-  @Test
-  void contextLoads() {
-    assertThat(problemAutoConfiguration).isNotNull();
-    assertThat(problemResolverConfiguration).isNotNull();
+    @Autowired(required = false)
+    private ProblemResolverConfiguration problemResolverConfiguration;
+
+    @Autowired(required = false)
+    private ProblemProperties properties;
+
+    @Test
+    void contextLoads() {
+      assertThat(problemAutoConfiguration).isNotNull();
+      assertThat(problemResolverConfiguration).isNotNull();
+
+      assertThat(properties).isNotNull();
+      assertThat(properties.isEnabled()).isTrue();
+    }
+  }
+
+  @SpringBootTest(
+      classes = {TestApp.class},
+      properties = {"problem4j.enabled=false"})
+  @Nested
+  class WithDisabled {
+
+    @Autowired(required = false)
+    private ProblemAutoConfiguration problemAutoConfiguration;
+
+    @Autowired(required = false)
+    private ProblemResolverConfiguration problemResolverConfiguration;
+
+    @Autowired(required = false)
+    private ProblemProperties properties;
+
+    @Test
+    void contextLoadsWithoutProblemConfiguration() {
+      assertThat(problemAutoConfiguration).isNull();
+      assertThat(problemResolverConfiguration).isNull();
+
+      assertThat(properties).isNull();
+    }
   }
 }
---
+++ problem4j-spring-webflux/src/test/java/io/github/malczuuu/problem4j/spring/webflux/error/ProblemErrorWebFluxConfigurationTest.java
@@ -0,0 +1,58 @@
+package io.github.malczuuu.problem4j.spring.webflux.error;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.malczuuu.problem4j.spring.webflux.ProblemWebFluxProperties;
+import io.github.malczuuu.problem4j.spring.webflux.app.WebFluxTestApp;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+
+class ProblemErrorWebFluxConfigurationTest {
+
+  @SpringBootTest(classes = {WebFluxTestApp.class})
+  @Nested
+  class WithEnabled {
+
+    @Autowired(required = false)
+    private ProblemErrorWebFluxConfiguration problemErrorWebFluxConfiguration;
+
+    @Autowired(required = false)
+    private ErrorWebExceptionHandler errorWebExceptionHandler;
+
+    @Autowired(required = false)
+    private ProblemWebFluxProperties properties;
+
+    @Test
+    void contextLoads() {
+      assertThat(problemErrorWebFluxConfiguration).isNotNull();
+      assertThat(errorWebExceptionHandler).isInstanceOf(ProblemErrorWebExceptionHandler.class);
+      assertThat(properties.getErrorWebExceptionHandler().isEnabled()).isTrue();
+    }
+  }
+
+  @SpringBootTest(
+      classes = {WebFluxTestApp.class},
+      properties = {"problem4j.webflux.error-web-exception-handler.enabled=false"})
+  @Nested
+  class WithDisabled {
+
+    @Autowired(required = false)
+    private ProblemErrorWebFluxConfiguration problemErrorWebFluxConfiguration;
+
+    @Autowired(required = false)
+    private ErrorWebExceptionHandler errorWebExceptionHandler;
+
+    @Autowired(required = false)
+    private ProblemWebFluxProperties properties;
+
+    @Test
+    void contextLoads() {
+      assertThat(problemErrorWebFluxConfiguration).isNull();
+      assertThat(errorWebExceptionHandler).isNotInstanceOf(ProblemErrorWebExceptionHandler.class);
+      assertThat(properties.getErrorWebExceptionHandler().isEnabled()).isFalse();
+    }
+  }
+}
